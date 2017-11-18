f7af9482b597f274e1011d7cf9d5513b0bf0ab4e
AJ Dugger <aj.dugger11@gmail.com>
2017-11-17 23:49:43 -0700

    adsf

--- test.js
+++ test.js
@@ -5,15 +5,25 @@ var black = document.getElementById("circle2");
 var score = document.getElementById("score");
 var counter = 0;
 var red = document.getElementById("ouch");
+var pageTimer = document.getElementById("time");
+var start = document.getElementById("start");
 var compare;
 var ouchCompare;
 var blackValue;
 var redValue;
 var blueValue;
+var timer;
 
-slider.oninput = function(){
+start.addEventListener("click",function(){
+    go();
+    counter=0;
+    score.innerHTML = counter;
+    timer = 15; 
+    
+    
+    slider.oninput = function(){
 	
-	 if (blue.getAttribute("r") == black.getAttribute("r")) {
+	 if (Math.abs(blue.getAttribute("r") - black.getAttribute("r")) <=5) {
         score.innerHTML = counter++;
 		blueValue = blue.getAttribute("r");
         blackValue = Math.floor(Math.random() * (135 - 2) ) + 2;
@@ -22,12 +32,12 @@ slider.oninput = function(){
         // create red ring
 		
 		if(blackValue > blueValue){
+            blackValue += 20;
 			redValue = Math.floor(Math.random() * (135 - blackValue) ) + blackValue;
 			red.setAttribute("r", redValue);
 		}
 		
-		if(blackValue < blueValue){
-			console.log(blueValue);
+		if(blackValue < blueValue){;
 			redValue = Math.floor(Math.random() * (150 - blueValue) ) + blueValue;
 			red.setAttribute("r", redValue);
 		}
@@ -35,9 +45,43 @@ slider.oninput = function(){
     else{
     	blue.setAttribute("r", this.value);
     }
-    if (blue.getAttribute("r") === red.getAttribute("r")) {
-    	alert("Game Over!");
+    if (Math.abs(blue.getAttribute("r") - red.getAttribute("r")) <= 2) {
+        timer = 1;
     }
 	
 	
+}
+
+});
+
+
+
+function reset(){
+    
+    blue.setAttribute("r", 135);
+    black.setAttribute("r", 1);
+    red.setAttribute("r", 0);
+    slider.oninput = null;
+    
+}
+
+function go (){ 
+    var x = setInterval(function() {
+        timer--;
+        start.innerHTML = timer;
+        
+        if (timer < 0) {
+            clearInterval(x); 
+            stop();
+        }
+    }, 1000);
+    
+    
+}
+
+function stop(){
+    timer = 0;
+    start.innerHTML = "Game Over - Play Again?";
+    reset(); 
+    
 }
